a9ecdd0e6d813ffb5c6e2f24fb3eab649861897c
Fatemeh Heidari <gabrina101@hotmail.com>
2022-06-06 14:46:26 +0430

    Update Startup.cs, convention routing

--- PartyInvites/Startup.cs
+++ PartyInvites/Startup.cs
@@ -37,6 +37,7 @@ namespace PartyInvites
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            //indicate convention routing, first type of routing
         {
             if (env.IsDevelopment())
             {
